b1af3b7fa2e58500e682af6d969019b3ccfced7b
mehedi-hasan22 <mehedi30554@gmail.com>
2022-09-04 20:41:02 +0600

    displayed news card

--- js/script.js
+++ js/script.js
@@ -24,38 +24,89 @@ const buttonClick = (btn) => {
     console.log('btn-clicked', btn);
 }
 
+var newsItemLength = 0;
 
-const loadNews = async (i) => {
-    const newsID = '0' + i;
+const loadNews = async (item) => {
+    const newsID = '0' + item;
     const newsUrl = `https://openapi.programming-hero.com/api/news/category/${newsID}`;
     const res = await fetch(newsUrl);
     const data = await res.json();
 
     const newsArrayLenght = data.data.length;
+    newsItemLength = newsArrayLenght;
+    displayNews(data.data)
+    console.log(data.data)
+}
 
-    const newsCards = document.getElementById('news-cards');
-    const newsDiv = document.createElement('div')
-    newsDiv.innerHTML = `
-    <div class="card my-3 container">
-        <div class="row">
+const displayNews = newsList => {
+    const newsContainer = document.getElementById('news-cards');
+    newsContainer.textContent = '';
+    newsList.forEach(news => {
+        let authorName = news.author.name;
+        if (authorName === null || authorName === 'system') {
+            authorName = 'no name available'
+        }
+        else {
+            authorName = news.author.name;
+        }
+        let newsView = news.total_view;
+        if (news.total_view === null) {
+            newsView = 'No'
+        }
+        const cardText = news.details
+        const slicedText = cardText.slice(1, 200) + '...';
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML = `<div class="card mt-5 container">
+        <div class="row g-0">
           <div class="col-md-4">
-            <img src="${data.data[0].thumbnail_url}" class="img-fluid rounded-start" alt="...">
+            <img src = "${news.image_url}" class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">${data.data[0].title}</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              <h5 class="card-title">${news.title}</h5>
+              <p class="card-text">${slicedText}</p>
             </div>
-          </div>
-        </div>
-    </div>
-    `
-    newsCards.appendChild(newsDiv);
+            <div class="d-flex justify-content-around">
+            <p><img src="${news.author.img}" style="max-width: 50px;" class="rounded-circle"> ${authorName}</p>
+            <p>${newsView} ${'views'}</p >
+            <p>${news.rating.number}${'⭐'}</p>
+            <button class="btn btn-outline-primary py-0"  data-bs-toggle="modal" data-bs-target="#newsDetailsModal">${'↩'}</button>
+            </div >
+          </div >
+        </div >
+      </div > `
+        newsContainer.appendChild(newsDiv)
+
+    });
 }
 
 loadNews();
 
 loadCatagory();
 
+displayNews();
+
+
+
+
+// const newsCards = document.getElementById('news-cards');
+// const newsDiv = document.createElement('div')
+// newsDiv.innerHTML = `
+// <div class="card my-3 container">
+//     <div class="row">
+//       <div class="col-md-4">
+//         <img src="${data.data[0].thumbnail_url}" class="img-fluid rounded-start" alt="...">
+//       </div>
+//       <div class="col-md-8">
+//         <div class="card-body">
+//           <h5 class="card-title">${data.data[0].title}</h5>
+//           <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+//             content. This content is a little bit longer.</p>
+//           <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+//         </div>
+//       </div>
+//     </div>
+// </div>
+// `
+// newsCards.appendChild(newsDiv);
